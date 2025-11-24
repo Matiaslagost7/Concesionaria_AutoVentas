d4f6bfa7b613a7977cf153a65411d53a7e1fd7f6
Matiaslagost7 <matiaslagost7@gmail.com>
2025-11-23 22:03:53 -0300

    Configurar settings.py para usar DATABASE_URL de Render y PostgreSQL en producción

--- concesionaria/concesionaria/settings.py
+++ concesionaria/concesionaria/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,10 +95,7 @@ WSGI_APPLICATION = 'concesionaria.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR / "db.sqlite3"}')
 }
 
 # Password validation
